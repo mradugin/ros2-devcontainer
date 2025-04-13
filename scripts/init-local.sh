4996d4b50be552117f7d87e6b392d21b4e6ebffa
Maksims Radugins <maxim@radugin.com>
2025-04-13 09:24:15 +0000

    Use placeholder file for .cache instead of script

--- scripts/init-local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Source logging functions
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source "$SCRIPT_DIR/logging.sh"
-
-info "Creating local cache dir..."
-mkdir -p .cache
